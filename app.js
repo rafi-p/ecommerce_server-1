3bc646a64daef4f29c7e04b655b8fe81f1b3fa30
RafiP <rafi.panji.p@gmail.com>
2020-11-11 11:03:33 +0700

    Push to new fork

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
   const errorHandler = require('./middlewares/errorHandler')
   require('dotenv').config()
 
+  const cors = require('cors')
   const router = require('./routes/index')
   const app = express()
   const PORT = process.env.PORT
   
+  app.use(cors())
   app.use(express.json())
   app.use(express.urlencoded({ extended: true }))
   
